715a21ddd8293088bafbb3b8848ca5ffa1511edd
KinLuu1368 <luukien323@gmail.com>
2023-03-08 07:53:33 +0700

    Lab6

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,3 +1,8 @@
-function onClick() {
-    var t = setTimeout("alert('hi');",1000);
-}
+let a = 10;
+//set a value of 10
+console.log(a);
+alert(a);
+//Nhập dữ liệu
+var x = prompt("Input n: ");
+//Xuất dữ liệu ra màn hình
+document.write(x);
